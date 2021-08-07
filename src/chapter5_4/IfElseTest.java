abf055b87951eaca9092629d38c82f289190db7d
Adopat <1615581829@qq.com>
2021-08-07 16:34:37 +0800

    update by Adopat

--- src/chapter5_4/IfElseTest.java
+++ src/chapter5_4/IfElseTest.java
@@ -2,6 +2,38 @@ package chapter5_4;
 
 public class IfElseTest {
     public static void main(String[] args){
+        test1(9);
+        System.out.println("======================");
+        test2(30);
+        System.out.println("======================");
+        test3(2);
+        System.out.println("======================");
+
+
+    }
+    public static void test1(int age){
+        if (age>0 && age<10){
+            System.out.println("年龄在10岁以内");
+        }
+    }
+    public static void test2(int age){
+        if (age>0 && age<10){
+            System.out.println("年龄在10随以内");
+        }else if(age>=10 && age<20){
+            System.out.println("年龄在10-20之间");
+        }else if (age>=20 && age<30){
+            System.out.println("年龄在20-30之间");
+
+        }else{
+            System.out.println("年龄在30岁以上");
+        }
+    }
+    public static void test3(int age){
+        if (age>0 && age<10){
+            System.out.println("年龄在10岁之间");
+        }else{
+            System.out.println("年龄大于10岁！！");
+        }
 
     }
 }
