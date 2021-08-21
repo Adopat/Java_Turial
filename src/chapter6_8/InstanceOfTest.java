5a167f260a1723c8691d32f82bfc272f1bc64563
Adopat <1615581829@qq.com>
2021-08-21 17:58:23 +0800

    update by Adopat

--- src/chapter6_8/InstanceOfTest.java
+++ src/chapter6_8/InstanceOfTest.java
@@ -1,11 +1,12 @@
 package chapter6_8;
-
+// Java实现多态有三个必要条件：继承(接口)、重写、向上转型。
 public class InstanceOfTest {
     public static void main(String[] args){
         Student student = new Student();
         // 判断 student 是否实现了Person接口
         boolean result = student instanceof Person;
         //System.out.println(result);
+        // 父类引用指向子类对象，因为子类重写了父类的方法，根据就近原则，优先调用子类的重写方法
         Person person1 = new Student();
         Person person2 = new Teacher();
         System.out.println("person1 instanceof Student:");
@@ -17,6 +18,10 @@ public class InstanceOfTest {
         System.out.println("===================================");
         person2.talk();
         System.out.println("=====================================");
+        // 向下转型，如果要调用子类特有的方法要进行向下转型
+        Student student11 = (Student) person1;
+        student11.run();
+
         if(person1 instanceof Student){
             // 类型强转
             Student student1 = (Student) person1;
--- src/chapter6_8/Student.java
+++ src/chapter6_8/Student.java
@@ -5,4 +5,7 @@ public class Student implements Person{
     public void talk(){
         System.out.println("学生在讲话");
     }
+    public void run(){
+        System.out.println("学生再跑");
+    }
 }
