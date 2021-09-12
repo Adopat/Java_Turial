b7e08324d670f44cc8ab1b517bbcc53f7cc08a7a
Adopat <1615581829@qq.com>
2021-09-12 21:29:06 +0800

    update by Adopat

---
+++ src/chapter12/TryCatchTest.java
@@ -0,0 +1,15 @@
+package chapter12;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * IO 流中异常的处理 JDK6以前的处理方法
+ */
+public class TryCatchTest {
+    public static void main(String[] args){
+
+    }
+}
