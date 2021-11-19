43dd14967e658d2076435fd72b3ca5dac489a0ae
Adopat <1615581829@qq.com>
2021-11-19 16:47:47 +0800

    update by Adopat

--- src/chapter4_7/VariableTypeTest.java
+++ src/chapter4_7/VariableTypeTest.java
@@ -1,7 +1,7 @@
 package chapter4_7;
 
 public class VariableTypeTest {
-    // static 修饰的为静态变量
+    // static 修饰的为静态变量 静态变量位类所拥有 ，所有对象共享
     public static String NAME;
     public static int AGE;
     public static  boolean FLAG;
@@ -20,6 +20,10 @@ public class VariableTypeTest {
         student.setName("justin");
         System.out.println(student.getAge()); // 10
         System.out.println(student.getName()); // justin
+        System.out.println("==================调用静态方法====================");
+        //aa();
+        testStatic();
+
 
     }
     public void aa(){
@@ -28,6 +32,10 @@ public class VariableTypeTest {
         System.out.println(VariableTypeTest.NAME);
         Student stu1 = new Student();
         System.out.println(stu1.gender);
+    }
+    public static void  testStatic(){
+        System.out.println("静态方法");
+
     }
 
 }
