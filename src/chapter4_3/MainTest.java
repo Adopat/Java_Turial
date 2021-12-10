4ff81f12c10eb22a9940612ab801051661a26c3a
Adopat <1615581829@qq.com>
2021-12-10 17:13:32 +0800

    update by Adopat

--- src/chapter4_3/MainTest.java
+++ src/chapter4_3/MainTest.java
@@ -1,5 +1,7 @@
 package chapter4_3;
 
+import java.io.UnsupportedEncodingException;
+
 public class MainTest {
     // byte 8位 static 必须要加，否则再main 方法中无法调用
     static byte b = 127;
@@ -18,7 +20,7 @@ public class MainTest {
     static boolean bool = false;
     // 16位
     static char ch = 'A';
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         MainTest mainTest = new MainTest();
         System.out.println(mainTest.c1); //100
         System.out.println(b);
@@ -44,11 +46,61 @@ public class MainTest {
         // 获取取值范围 使用对应的包装类
         System.out.println("===================================");
         test();
+        // 进制转化
+        System.out.println("===========进制转化========================");
+        test1();
+        // 字符乱码解决方案
+        System.out.println("=============字符乱码解决方案=====================");
+        recover("鎻掑叆 / 鏇存柊");
+        String testString = "鎻掑叆 / 鏇存柊";
+        // getBytes() 解码 new String() 编码
+        String resultString = new String(testString.getBytes("GB18030"),"utf-8");
+        System.out.println("resultString= "+resultString);
+        System.out.println("===============================char================");
+        testChar();
     }
     public static void test(){
         System.out.println("byte 取值范围："+Byte.MIN_VALUE+"-"+Byte.MAX_VALUE); //byte 取值范围：-128-127
         System.out.println("short 取值范围："+Short.MIN_VALUE+"-"+Short.MAX_VALUE);// short 取值范围：-32768-32767
         System.out.println("int 取值范围："+Integer.MIN_VALUE+"-"+Integer.MAX_VALUE);// int 取值范围：-2147483648-2147483647
         System.out.println("long 取值范围："+Long.MIN_VALUE+"-"+Long.MAX_VALUE);//long 取值范围：-9223372036854775808-9223372036854775807
+    }
+    // 查看整数的二进制和十六进制 使用 Integer 和 Long 的方法 进行进制转化
+    public static void test1(){
+        int a = 10;
+        System.out.println("10进制转二进制 ："+Integer.toBinaryString(a));
+        System.out.println("10进制转16进制： "+Integer.toHexString(a));
+        System.out.println("10进制转二进制 ："+Long.toBinaryString(a));
+        System.out.println("10进制转16进制： "+Integer.toHexString(a));
+
+    }
+    // 解决字符编码
+    public static void recover(String str) throws UnsupportedEncodingException {
+        String[] charsets = new String[]{"windows-1252","GB18030","Big5","UTF-8"};
+        for(int i=0;i<charsets.length;i++){
+            for(int j=0;j<charsets.length;j++){
+                if(i!=j){
+                    String s = new String(str.getBytes(charsets[i]),charsets[j]);
+                    System.out.println("---- 原来编码(A)假设是: "+charsets[j]+", 被错误解读为了(B): "+charsets[i]);
+                    System.out.println(s);
+                    System.out.println();
+                }
+            }
+        }
+    }
+    // char 的本质是一个固定占用两个字节的无符号正整数,这个正整数对应Unicode编号
+    public static void testChar(){
+        char a = 'A';
+        int c = (a+32);
+        System.out.println("c= "+(char)c);
+        char d = 65;
+        System.out.println("d= "+d);
+        byte b = 127;
+        // 出现-128的原因是因为 128超过了byte 的表示范围 当计算结果超出表示范围的时候，最高位往往是1，然后就会被看做负数。比如说，127+1：
+        // 1111111 + 0000001 = 10000000 最高位为1 当成负数看待了
+        // 负数采用 补码表示   原码取反+1 = 补码
+        byte bb = (byte)(127 + 1);
+        System.out.println("bb="+bb);
+
     }
 }
--- src/chapter5_5/SwitchTest.java
+++ src/chapter5_5/SwitchTest.java
@@ -1,5 +1,5 @@
 package chapter5_5;
-
+// 再多条件的情况下，应该使用 swtich case 提高效率 ，条件执行的本质 是依赖月条件跳转，无条件跳转，跳转表
 public class SwitchTest {
     public static void main(String[] args){
         int day = 1;
@@ -21,6 +21,10 @@ public class SwitchTest {
         }
         System.out.println(content);
         test1("周一");
+        System.out.println("===========swtich 穿透测试 test2() 运行===========================");
+        test2(1);
+        System.out.println("===========switch 堆叠==================");
+        test3(3);
 
     }
     // 在JDK7后 switch 中可以使用字符串
@@ -41,5 +45,30 @@ public class SwitchTest {
                 System.out.println("无效日期！！！");
         }
         System.out.println("今天是星期 "+dayNumber);
+    }
+    // switch 记得每个case 带break ,否则会引起 switch 穿透 输出 1，2，3
+    public static void test2(int day){
+        switch(day){
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+            default:
+                System.out.println("3");
+        }
+    }
+    // case 堆叠
+    public static void test3(int day){
+        switch (day){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                System.out.println(day);
+                break;
+            case 4:
+            case 5:
+        }
     }
 }
